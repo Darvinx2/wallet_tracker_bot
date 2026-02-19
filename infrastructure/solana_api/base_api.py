8b0a4eacd51aac56de56429f7f17d82138c0439f
solmi <darvineng228@gmail.com>
2026-02-20 01:41:57 +0300

    update base_api.py file

--- infrastructure/solana_api/base_api.py
+++ infrastructure/solana_api/base_api.py
@@ -15,26 +15,5 @@ class BaseApiClient(ABC):
         try:
             data = await response.json()
         except Exception as e:
-            logger.error(f"{api_name} error: {e}")
-            raise Exception("Возникла ошибка парсинга")
-        if response.status == 400:
-            logger.error(f"{api_name}: Error 400")
-            raise Exception(f"{api_name}: Error 400")
-        elif response.status == 401:
-            logger.error(f"{api_name}: Error 401")
-            raise Exception(f"{api_name}: Error 401")
-        elif response.status == 404:
-            logger.error(f"{api_name}: Error 404")
-            raise Exception(f"{api_name}: Error 404")
-        elif response.status == 429:
-            logger.error(f"{api_name}: Error 429")
-            raise Exception(f"{api_name}: Error 429")
-        elif response.status == 500:
-            logger.error(f"{api_name}: Error 500")
-            raise Exception(f"{api_name}: Error 500")
-        elif response.status == 200:
-            logger.info(f"{api_name}: Status 200")
-            return data
-        else:
-            logger.error(f"{api_name}: Status {response.status}")
-            raise Exception(f"Неожиданный статус: {response.status}")
+            logger.error(f"{api_name} error {response.status}: {e}")
+            raise Exception(f"{api_name}: Error {response.status}")
